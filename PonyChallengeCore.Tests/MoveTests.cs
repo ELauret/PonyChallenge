cc1c5328f0bb6ce2a43067831d783491cfc4e5c2
Eric Lauret <eric.lauret@live.com>
2019-10-24 12:26:40 +0200

    Optimized solving logic, taking into account distance to exit
    Related Work Items: #223

--- PonyChallengeCore.Tests/MoveTests.cs
+++ PonyChallengeCore.Tests/MoveTests.cs
@@ -1,6 +1,7 @@
-using PonyChallengeCore;
+using PonyChallengeCore.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using System;
 
 namespace PonyChallengeCore.Tests
 {
@@ -11,43 +12,79 @@ namespace PonyChallengeCore.Tests
         [ExpectedException(typeof(IOException))]
         public void DetermineMoveTestInvalidKey()
         {
-            Program.DetermineMove(System.ConsoleKey.Escape);
+            Program.DetermineMove(ConsoleKey.Escape);
         }
 
         [TestMethod()]
-        public void DetermineMoveTestValidKeyUpArrow()
+        public void DetermineMoveWithUserInputTestValidKeyUpArrow()
         {
-            var move = Program.DetermineMove(System.ConsoleKey.UpArrow);
+            var actual = Program.DetermineMove(ConsoleKey.UpArrow);
             var expected = "north";
 
-            Assert.AreEqual(expected, move);
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
-        public void DetermineMoveTestValidKeyLeftArrow()
+        public void DetermineMoveWithUserInputTestValidKeyLeftArrow()
         {
-            var move = Program.DetermineMove(System.ConsoleKey.LeftArrow);
+            var actual = Program.DetermineMove(ConsoleKey.LeftArrow);
             var expected = "west";
 
-            Assert.AreEqual(expected, move);
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
-        public void DetermineMoveTestValidKeyDownArrow()
+        public void DetermineMoveWithUserInputTestValidKeyDownArrow()
         {
-            var move = Program.DetermineMove(System.ConsoleKey.DownArrow);
+            var actual = Program.DetermineMove(ConsoleKey.DownArrow);
             var expected = "south";
 
-            Assert.AreEqual(expected, move);
+            Assert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
-        public void DetermineMoveTestValidKeyRightArrow()
+        public void DetermineMoveWithUserInputTestValidKeyRightArrow()
         {
-            var move = Program.DetermineMove(System.ConsoleKey.RightArrow);
+            var actual = Program.DetermineMove(ConsoleKey.RightArrow);
             var expected = "east";
 
-            Assert.AreEqual(expected, move);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void DetermineMoveProgramaticallyTestValidSideNorth()
+        {
+            var actual = Program.DetermineMove(CellSide.North);
+            var expected = "north";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void DetermineMoveProgramaticallyTestValidSideWest()
+        {
+            var actual = Program.DetermineMove(CellSide.West);
+            var expected = "west";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void DetermineMoveProgramaticallyTestValidSideSouth()
+        {
+            var actual = Program.DetermineMove(CellSide.South);
+            var expected = "south";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void DetermineMoveProgramaticallyTestValidSideEast()
+        {
+            var actual = Program.DetermineMove(CellSide.East);
+            var expected = "east";
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
